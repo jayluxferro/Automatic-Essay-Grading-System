c73232b400441191f2ea5ecb9dfd85c8ac5db4ef
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2019-11-18 06:57:54 +0000

    added embeddings

--- sample_run.sh
+++ sample_run.sh
@@ -1,1 +1,2 @@
-python train.py -tr data/fold_0/train.tsv -tu data/fold_0/dev.tsv -ts data/fold_0/test.tsv -p 1 --emb embeddings.w2v.txt -o output_dir --cnndim 50 -t reg
+#python train.py -tr data/fold_0/train.tsv -tu data/fold_0/dev.tsv -ts data/fold_0/test.tsv -p 1 --emb embeddings.w2v.txt -o output_dir --cnndim 50 -t reg
+python train.py -tr data/fold_0/train.tsv -tu data/fold_0/dev.tsv -ts data/fold_0/test.tsv -p 1 -o output_dir --cnndim 50 --epochs 5
