ea55319398290701ed3371db9ac41c7f48d6fe31
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2019-12-10 08:49:24 +0000

    updated run script

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 # all prompts with lstm
-python train.py -tr data/fold_0/train.tsv -tu data/fold_0/dev.tsv -ts data/fold_0/test.tsv -p 0 --emb embeddings.w2v.txt -o output_dir
-zip -r scn1.zip output_dir/
+for x in $(seq 1 8);
+do
+  python train.py -tr data/fold_0/train.tsv -tu data/fold_0/dev.tsv -ts data/fold_0/test.tsv -p $x --emb embeddings.w2v.txt -o output_dir -u lstm
+  zip -r scn-lstm-$x.zip output_dir/
+done
 
 # all prompts with gru
-python train.py -tr data/fold_0/train.tsv -tu data/fold_0/dev.tsv -ts data/fold_0/test.tsv -p 0 --emb embeddings.w2v.txt -o output_dir -u gru
-zip -r scn2.zip output_dir/
+for x in $(seq 1 8);
+do
+  python train.py -tr data/fold_0/train.tsv -tu data/fold_0/dev.tsv -ts data/fold_0/test.tsv -p $x --emb embeddings.w2v.txt -o output_dir -u gru
+  zip -r scn-gru-$x.zip output_dir/
+done
