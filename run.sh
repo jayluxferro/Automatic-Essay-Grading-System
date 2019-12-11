8954e75e3817634d8274b31173005b97f6545bfc
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2019-12-11 08:58:37 +0000

    updated run script

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 # all prompts with lstm
 for x in $(seq 1 8);
 do
+  # preprocess data
+  cd data && python3 preprocess_asap.py -i training_set_rel3.tsv
+  cd ..
   python train.py -tr data/fold_0/train.tsv -tu data/fold_0/dev.tsv -ts data/fold_0/test.tsv -p $x --emb embeddings.w2v.txt -o output_dir -u lstm
   zip -r scn-lstm-$x.zip output_dir/
 done
@@ -8,6 +11,8 @@ done
 # all prompts with gru
 for x in $(seq 1 8);
 do
+  cd data && python3 preprocess_asap.py -i training_set_rel3.tsv
+  cd ..
   python train.py -tr data/fold_0/train.tsv -tu data/fold_0/dev.tsv -ts data/fold_0/test.tsv -p $x --emb embeddings.w2v.txt -o output_dir -u gru
   zip -r scn-gru-$x.zip output_dir/
 done
