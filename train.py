e86d8b80c8be96d964cdacdd7fce3982eccc3902
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2019-12-12 14:26:09 +0000

    changed codebase

--- train.py
+++ train.py
@@ -1,255 +1,97 @@
-#!/usr/bin/env python
+"""
+Author: Jay Lux Ferro
+Date:   12 Dec 2018
+Task:   AEGS
+"""
 
-import argparse
-import logging
-import numpy as np
-import scipy
-from time import time
-import sys
-import lux.utils as U
-import pickle as pk
+### definitions ####
+dataset_dir = './data'
+
+### imports     ####
 import os
+import pandas as pd
+from sklearn.cross_validation import KFold
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import cohen_kappa_score
+
+X = pd.read_csv(os.path.join(dataset_dir, 'training_set_rel3.tsv'), sep='\t', encoding='ISO-8859-1')
+y = X['domain1_score']
+X = X.dropna(axis=1)
+X = X.drop(columns=['rater1_domain1', 'rater2_domain1'])
+
+# show data; just for fun
+X.head()
 
 
-os.environ['KERAS_BACKEND'] = 'theano'
-#os.environ['THEANO_FLAGS'] = 'device=cuda' # cpu, cuda0
-#os.environ['THEANO_FLAGS'] = 'device=gpu0,floatX=float32' # cpu, cuda0
-#os.environ['OMP_NUM_THREADS'] = '32'
-#os.environ['openmp'] = 'True'
-#import theano.sandbox.cuda
-#theano.sandbox.cuda.use("gpu0")
+# training phase # K-Fold cross validation ( K = 5)
+# QWK
 
-logger = logging.getLogger(__name__)
+cv = KFold(len(X), n_folds=5, shuffle=True)
+results = []
+y_pred_list = []
 
-###############################################################################################################################
-## Parse arguments
-#
+count = 1
+for traincv, testcv in cv:
+    print("\n--------Fold {}--------\n".format(count))
+    X_test, X_train, y_test, y_train = X.iloc[testcv], X.iloc[traincv], y.iloc[testcv], y.iloc[traincv]
+    
+    train_essays = X_train['essay']
+    test_essays = X_test['essay']
+    
+    sentences = []
+    
+    for essay in train_essays:
+            # Obtaining all sentences from the training essays.
+            sentences += essay_to_sentences(essay, remove_stopwords = True)
+            
+    # Initializing variables for word2vec model.
+    num_features = 300 
+    min_word_count = 40
+    num_workers = 4
+    context = 10
+    downsampling = 1e-3
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-tr", "--train", dest="train_path", type=str, metavar='<str>', required=True, help="The path to the training set")
-parser.add_argument("-tu", "--tune", dest="dev_path", type=str, metavar='<str>', required=True, help="The path to the development set")
-parser.add_argument("-ts", "--test", dest="test_path", type=str, metavar='<str>', required=True, help="The path to the test set")
-parser.add_argument("-o", "--out-dir", dest="out_dir_path", type=str, metavar='<str>', required=True, help="The path to the output directory")
-parser.add_argument("-p", "--prompt", dest="prompt_id", type=int, metavar='<int>', required=False, help="Promp ID for ASAP dataset. '0' means all prompts.")
-parser.add_argument("-t", "--type", dest="model_type", type=str, metavar='<str>', default='regp', help="Model type (reg|regp|breg|bregp) (default=regp)")
-parser.add_argument("-u", "--rec-unit", dest="recurrent_unit", type=str, metavar='<str>', default='lstm', help="Recurrent unit type (lstm|gru|simple) (default=lstm)")
-parser.add_argument("-a", "--algorithm", dest="algorithm", type=str, metavar='<str>', default='rmsprop', help="Optimization algorithm (rmsprop|sgd|adagrad|adadelta|adam|adamax) (default=rmsprop)")
-parser.add_argument("-l", "--loss", dest="loss", type=str, metavar='<str>', default='mse', help="Loss function (mse|mae) (default=mse)")
-parser.add_argument("-e", "--embdim", dest="emb_dim", type=int, metavar='<int>', default=50, help="Embeddings dimension (default=50)")
-parser.add_argument("-c", "--cnndim", dest="cnn_dim", type=int, metavar='<int>', default=0, help="CNN output dimension. '0' means no CNN layer (default=0)")
-parser.add_argument("-w", "--cnnwin", dest="cnn_window_size", type=int, metavar='<int>', default=3, help="CNN window size. (default=3)")
-parser.add_argument("-r", "--rnndim", dest="rnn_dim", type=int, metavar='<int>', default=300, help="RNN dimension. '0' means no RNN layer (default=300)")
-parser.add_argument("-b", "--batch-size", dest="batch_size", type=int, metavar='<int>', default=32, help="Batch size (default=32)")
-parser.add_argument("-v", "--vocab-size", dest="vocab_size", type=int, metavar='<int>', default=4000, help="Vocab size (default=4000)")
-parser.add_argument("--aggregation", dest="aggregation", type=str, metavar='<str>', default='mot', help="The aggregation method for regp and bregp types (mot|attsum|attmean) (default=mot)")
-parser.add_argument("--dropout", dest="dropout_prob", type=float, metavar='<float>', default=0.5, help="The dropout probability. To disable, give a negative number (default=0.5)")
-parser.add_argument("--vocab-path", dest="vocab_path", type=str, metavar='<str>', help="(Optional) The path to the existing vocab file (*.pkl)")
-parser.add_argument("--skip-init-bias", dest="skip_init_bias", action='store_true', help="Skip initialization of the last layer bias")
-parser.add_argument("--emb", dest="emb_path", type=str, metavar='<str>', help="The path to the word embeddings file (Word2Vec format)")
-parser.add_argument("--epochs", dest="epochs", type=int, metavar='<int>', default=50, help="Number of epochs (default=50)")
-parser.add_argument("--maxlen", dest="maxlen", type=int, metavar='<int>', default=0, help="Maximum allowed number of words during training. '0' means no limit (default=0)")
-parser.add_argument("--seed", dest="seed", type=int, metavar='<int>', default=1234, help="Random seed (default=1234)")
-args = parser.parse_args()
+    print("Training Word2Vec Model...")
+    model = Word2Vec(sentences, workers=num_workers, size=num_features, min_count = min_word_count, window = context, sample = downsampling)
 
-out_dir = args.out_dir_path
+    model.init_sims(replace=True)
+    model.wv.save_word2vec_format('word2vecmodel.bin', binary=True)
 
-U.mkdir_p(out_dir + '/preds')
-U.set_logger(out_dir)
-U.print_args(args)
+    clean_train_essays = []
+    
+    # Generate training and testing data word vectors.
+    for essay_v in train_essays:
+        clean_train_essays.append(essay_to_wordlist(essay_v, remove_stopwords=True))
+    trainDataVecs = getAvgFeatureVecs(clean_train_essays, model, num_features)
+    
+    clean_test_essays = []
+    for essay_v in test_essays:
+        clean_test_essays.append(essay_to_wordlist( essay_v, remove_stopwords=True ))
+    testDataVecs = getAvgFeatureVecs( clean_test_essays, model, num_features )
+    
+    trainDataVecs = np.array(trainDataVecs)
+    testDataVecs = np.array(testDataVecs)
+    # Reshaping train and test vectors to 3 dimensions. (1 represents one timestep)
+    trainDataVecs = np.reshape(trainDataVecs, (trainDataVecs.shape[0], 1, trainDataVecs.shape[1]))
+    testDataVecs = np.reshape(testDataVecs, (testDataVecs.shape[0], 1, testDataVecs.shape[1]))
+    
+    lstm_model = get_model()
+    lstm_model.fit(trainDataVecs, y_train, batch_size=64, epochs=50)
+    #lstm_model.load_weights('./model_weights/final_lstm.h5')
+    y_pred = lstm_model.predict(testDataVecs)
+    
+    # Save any one of the 8 models.
+    if count == 5:
+         lstm_model.save('./model_weights/final_lstm.h5')
+    
+    # Round y_pred to the nearest integer.
+    y_pred = np.around(y_pred)
+    
+    # Evaluate the model on the evaluation metric. "Quadratic mean averaged Kappa"
+    result = cohen_kappa_score(y_test.values,y_pred,weights='quadratic')
+    print("Kappa Score: {}".format(result))
+    results.append(result)
 
-assert args.model_type in {'reg', 'regp', 'breg', 'bregp'}
-assert args.algorithm in {'rmsprop', 'sgd', 'adagrad', 'adadelta', 'adam', 'adamax'}
-assert args.loss in {'mse', 'mae'}
-assert args.recurrent_unit in {'lstm', 'gru', 'simple'}
-assert args.aggregation in {'mot', 'attsum', 'attmean'}
+    count += 1
 
-if args.seed > 0:
-	np.random.seed(args.seed)
-
-if args.prompt_id:
-	from lux.asap_evaluator import Evaluator
-	import lux.asap_reader as dataset
-else:
-	raise NotImplementedError
-
-###############################################################################################################################
-## Prepare data
-#
-
-from keras.preprocessing import sequence
-
-# data_x is a list of lists
-(train_x, train_y, train_pmt), (dev_x, dev_y, dev_pmt), (test_x, test_y, test_pmt), vocab, vocab_size, overal_maxlen, num_outputs = dataset.get_data(
-	(args.train_path, args.dev_path, args.test_path), args.prompt_id, args.vocab_size, args.maxlen, tokenize_text=True, to_lower=True, sort_by_len=False, vocab_path=args.vocab_path)
-
-# Dump vocab
-with open(out_dir + '/vocab.pkl', 'wb') as vocab_file:
-	pk.dump(vocab, vocab_file)
-
-# Pad sequences for mini-batch processing
-if args.model_type in {'breg', 'bregp'}:
-	assert args.rnn_dim > 0
-	assert args.recurrent_unit == 'lstm'
-	train_x = sequence.pad_sequences(train_x, maxlen=overal_maxlen)
-	dev_x = sequence.pad_sequences(dev_x, maxlen=overal_maxlen)
-	test_x = sequence.pad_sequences(test_x, maxlen=overal_maxlen)
-else:
-	train_x = sequence.pad_sequences(train_x)
-	dev_x = sequence.pad_sequences(dev_x)
-	test_x = sequence.pad_sequences(test_x)
-
-###############################################################################################################################
-## Some statistics
-#
-
-import keras.backend as K
-
-train_y = np.array(train_y, dtype=K.floatx())
-dev_y = np.array(dev_y, dtype=K.floatx())
-test_y = np.array(test_y, dtype=K.floatx())
-
-if args.prompt_id:
-	train_pmt = np.array(train_pmt, dtype='int32')
-	dev_pmt = np.array(dev_pmt, dtype='int32')
-	test_pmt = np.array(test_pmt, dtype='int32')
-
-bincounts, mfs_list = U.bincounts(train_y)
-with open('%s/bincounts.txt' % out_dir, 'w') as output_file:
-	for bincount in bincounts:
-		output_file.write(str(bincount) + '\n')
-
-train_mean = train_y.mean(axis=0)
-train_std = train_y.std(axis=0)
-dev_mean = dev_y.mean(axis=0)
-dev_std = dev_y.std(axis=0)
-test_mean = test_y.mean(axis=0)
-test_std = test_y.std(axis=0)
-
-logger.info('Statistics:')
-
-logger.info('  train_x shape: ' + str(np.array(train_x).shape))
-logger.info('  dev_x shape:   ' + str(np.array(dev_x).shape))
-logger.info('  test_x shape:  ' + str(np.array(test_x).shape))
-
-logger.info('  train_y shape: ' + str(train_y.shape))
-logger.info('  dev_y shape:   ' + str(dev_y.shape))
-logger.info('  test_y shape:  ' + str(test_y.shape))
-
-logger.info('  train_y mean: %s, stdev: %s, MFC: %s' % (str(train_mean), str(train_std), str(mfs_list)))
-
-# We need the dev and test sets in the original scale for evaluation
-dev_y_org = dev_y.astype(dataset.get_ref_dtype())
-test_y_org = test_y.astype(dataset.get_ref_dtype())
-
-# Convert scores to boundary of [0 1] for training and evaluation (loss calculation)
-train_y = dataset.get_model_friendly_scores(train_y, train_pmt)
-dev_y = dataset.get_model_friendly_scores(dev_y, dev_pmt)
-test_y = dataset.get_model_friendly_scores(test_y, test_pmt)
-
-###############################################################################################################################
-## Optimizer algorithm
-#
-
-from lux.optimizers import get_optimizer
-
-optimizer = get_optimizer(args)
-
-###############################################################################################################################
-## Building model
-#
-
-from lux.models import create_model
-
-if args.loss == 'mse':
-	loss = 'mean_squared_error'
-	metric = 'mean_absolute_error'
-else:
-	loss = 'mean_absolute_error'
-	metric = 'mean_squared_error'
-
-model = create_model(args, train_y.mean(axis=0), overal_maxlen, vocab)
-model.compile(loss=loss, optimizer=optimizer, metrics=[metric])
-
-###############################################################################################################################
-## Plotting model
-#
-
-from keras.utils.vis_utils import plot_model
-
-plot_model(model, to_file = out_dir + '/model.png')
-
-
-###############################################################################################################################
-## Model summary
-model.summary()
-logger.info('Saving model summary')
-with open(out_dir + '/model_summary.txt', 'w') as ms:
-    model.summary(print_fn=lambda x: ms.write(x + '\n'))
-logger.info(' Done')
-
-###############################################################################################################################
-## Save model architecture
-#
-
-logger.info('Saving model architecture')
-with open(out_dir + '/model_arch.json', 'w') as arch:
-	arch.write(model.to_json(indent=2))
-logger.info('  Done')
-
-###############################################################################################################################
-## Evaluator
-#
-
-evl = Evaluator(dataset, args.prompt_id, out_dir, dev_x, test_x, dev_y, test_y, dev_y_org, test_y_org)
-
-###############################################################################################################################
-## Training
-#
-
-logger.info('--------------------------------------------------------------------------------------------------------------------------')
-logger.info('Initial Evaluation:')
-evl.evaluate(model, -1, print_info=True)
-
-total_train_time = 0
-total_eval_time = 0
-
-results_data = ""
-for ii in range(args.epochs):
-	# Training
-	t0 = time()
-	train_history = model.fit(train_x, train_y, batch_size=args.batch_size, epochs=1, verbose=1)
-	tr_time = time() - t0
-	total_train_time += tr_time
-
-	# Evaluate
-	t0 = time()
-	evl.evaluate(model, ii)
-	evl_time = time() - t0
-	total_eval_time += evl_time
-
-	# Print information
-	train_loss = train_history.history['loss'][0]
-	train_metric = train_history.history[metric][0]
-	logger.info('Epoch %d, train: %is, evaluation: %is' % (ii, tr_time, evl_time))
-	logger.info('[Train] loss: %.4f, metric: %.4f' % (train_loss, train_metric))
-	evl.print_info()
-
-        # data format
-        # epoch, train_loss, train_metric, dev_loss, dev_metric, test_loss, test_metric, dev_qwk, test_qwk
-        results_data += "%d,%.4f,%.4f," % (ii + 1, train_loss, train_metric)
-        results_data += evl.return_info()
-
-###############################################################################################################################
-## Summary of the results
-#
-
-logger.info('Training:   %i seconds in total' % total_train_time)
-logger.info('Evaluation: %i seconds in total' % total_eval_time)
-
-evl.print_final_info()
-
-# save data to file
-logger.info(' Saving results to file')
-with open(out_dir + '/results.txt', 'w') as rs:
-    rs.write(results_data)
-logger.info(' Done')
+print("Average Kappa score after a 5-fold cross validation: ",np.around(np.array(results).mean(),decimals=4))
